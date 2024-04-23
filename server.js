f93447610793bc9f12be65d93dc6393f7fc160a9
jenniferchristine <jeja2306@student.miun.se>
2024-04-23 17:25:41 +0200

    async function to connect to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 
 // init express
 const app = express();
@@ -8,9 +9,19 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// självkörande funktion för att hantera anslutning
+(async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/cv");
+        console.log("Connected to MongoDB...");
+    } catch (error) {
+        console.error("Error when connecting to the database:", error);
+    }
+})();
+
 // routes
 app.get("/cv", async (req, res) => {
-    res.json({message: "API för arbetserfarenheter"});
+    res.json({ message: "API för arbetserfarenheter" });
 });
 
 // starta server
